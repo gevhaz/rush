d555acddcc3f96f0273f5244d12b87c3772c4f2b
Pontus Laos <laos.pontus@gmail.com>
2022-12-05 17:11:43 +0100

    WIP: Add support for Engine contained in memory
    We had halfway support for this before, but it never worked. Now, the
    execution is implemented on each generic `Engine`, making it possible to
    write the output to a `Vec<u8>` if in memory, otherwise as intended.
    
    This can be used to verify that our parsing/execution works as expected,
    as part of a regression suite. Although it doesn't test the actual
    `Engine<std::io::Stdout>` that's used in the REPL, it should be good
    enough for now.

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -12,35 +12,11 @@ use crate::{path, Result};
 pub use self::history::History;
 use self::parser::ast::{parse, CommandType, SyntaxTree};
 
-pub fn read_and_execute<W: Write>(engine: &mut Engine<W>) -> Result<Vec<ExitStatus>> {
-    let line = read_line(engine)?;
-    let ast = parse(line);
-    walk_ast(engine, ast)
-}
-
-fn walk_ast<W: Write>(engine: &mut Engine<W>, ast: SyntaxTree) -> Result<Vec<ExitStatus>> {
-    ast.commands()
-        .iter()
-        .fold(Ok(vec![]), |_, c| engine.execute(c))
-}
-
 pub struct Engine<W: Write> {
     pub writer: W,
     pub prev_dir: Option<PathBuf>,
     pub commands: Vec<String>,
     pub history: History,
-}
-
-impl Engine<Vec<u8>> {
-    pub fn in_memory() -> Self {
-        let history = History::init().expect("could not initialize history");
-        Self {
-            writer: Vec::new(),
-            prev_dir: None,
-            commands: path::get_cmds_from_path(),
-            history,
-        }
-    }
 }
 
 impl<W: Write> Engine<W> {
@@ -68,6 +44,35 @@ impl<W: Write> Engine<W> {
         // This is the place in which we will expand subcommands,
         // variables, globs, and tildes
         todo!()
+    }
+}
+
+impl Engine<Stdout> {
+    pub fn new() -> Self {
+        let history = History::init().expect("could not initialize history");
+        Self {
+            prev_dir: None,
+            writer: io::stdout(),
+            commands: path::get_cmds_from_path(),
+            history,
+        }
+    }
+
+    pub fn read_and_execute(&mut self) -> Result<Vec<ExitStatus>> {
+        let line = read_line(self)?;
+        let ast = parse(line);
+        self.walk_ast(ast)
+    }
+
+    pub fn execute_line(&mut self, line: impl ToString) -> Result<Vec<ExitStatus>> {
+        let ast = parse(line.to_string());
+        self.walk_ast(ast)
+    }
+
+    fn walk_ast(&mut self, ast: SyntaxTree) -> Result<Vec<ExitStatus>> {
+        ast.commands()
+            .iter()
+            .fold(Ok(vec![]), |_, c| self.execute(c))
     }
 
     pub fn execute(&mut self, cmd: &CommandType) -> Result<Vec<ExitStatus>> {
@@ -117,14 +122,77 @@ impl<W: Write> Engine<W> {
     }
 }
 
-impl Engine<Stdout> {
-    pub fn new() -> Self {
+impl Engine<Vec<u8>> {
+    pub fn in_memory() -> Self {
         let history = History::init().expect("could not initialize history");
         Self {
+            writer: Vec::new(),
             prev_dir: None,
-            writer: io::stdout(),
             commands: path::get_cmds_from_path(),
             history,
+        }
+    }
+
+    pub fn output(&self) -> std::borrow::Cow<str> {
+        String::from_utf8_lossy(&self.writer)
+    }
+    
+    pub fn execute_line(&mut self, line: impl ToString) -> Result<()> {
+        let ast = parse(line.to_string());
+        self.walk_ast(ast)
+    }
+
+    fn walk_ast(&mut self, ast: SyntaxTree) -> Result<()> {
+        ast.commands().iter().fold(Ok(()), |_, c| self.execute(c))
+    }
+
+    fn execute(&mut self, cmd: &CommandType) -> Result<()> {
+        match cmd {
+            CommandType::Single(cmd) => {
+                let child = process::Command::new(cmd.cmd_name())
+                    .args(cmd.args())
+                    .output()?;
+
+                write!(self.writer, "{}", String::from_utf8_lossy(&child.stdout))?;
+
+                // let code = result.status.code().unwrap_or_default();
+                // Ok(vec![ExitStatus::from(code)])
+
+                Ok(())
+            }
+
+            CommandType::Pipeline(cmds) => {
+                let mut prev_result: Option<(Option<ChildStdout>, i32)> = None;
+
+                for (i, cmd) in cmds.iter().enumerate() {
+                    let stdin = match prev_result {
+                        Some((Some(stdout), _)) => Stdio::from(stdout),
+                        _ => Stdio::inherit(),
+                    };
+
+                    if i == cmds.len() - 1 {
+                        let mut child = process::Command::new(cmd.cmd_name())
+                            .args(cmd.args())
+                            .stdin(stdin)
+                            .output()?;
+                        self.writer.append(&mut child.stdout);
+                        break;
+                    } else {
+                        let mut child = process::Command::new(cmd.cmd_name())
+                            .args(cmd.args())
+                            .stdin(stdin)
+                            .stdout(Stdio::piped())
+                            .spawn()?;
+
+                        prev_result = Some((child.stdout.take(), 0));
+                    }
+
+                    // let result = child.wait_with_output()?;
+                    // statuses.push(ExitStatus::from(result.status.code().unwrap_or_default()));
+                }
+
+                Ok(())
+            }
         }
     }
 }
@@ -142,5 +210,20 @@ pub struct ExitStatus {
 impl ExitStatus {
     pub fn from(code: i32) -> Self {
         Self { code }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_commands_work() {
+        let mut engine = Engine::in_memory();
+
+        engine.execute_line("echo oof | rev; echo bar").unwrap();
+        engine.execute_line("echo baz").unwrap();
+
+        assert_eq!("foo\nbar\nbaz\n", engine.output());
     }
 }
